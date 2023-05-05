c9358255f1882d7e76066328a80e03ee23d067eb
Tim Walshjamin <walshta@users.noreply.github.com>
2023-05-05 16:07:33 -0400

    remove unused variable

--- src/app/ambassadors/ambassador.service.spec.ts
+++ src/app/ambassadors/ambassador.service.spec.ts
@@ -1,11 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AmbassadorService } from './ambassador.service';
-import { Ambassador } from "./ambassador";
 
 describe('AmbassadorService', () => {
   let service: AmbassadorService;
-  let ambassadors: Ambassador[];
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
