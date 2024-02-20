42a2ec23cabbc6eab3654c4cc5cf91b0421eee28
Tim Walshjamin <walshta@users.noreply.github.com>
2024-02-20 16:57:41 -0500

    update to angular 17 standards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title: string = 'Thank You Matcher';
-  mods = [
-    {
-      icon: 'school',
-      name: 'Ambassadors',
-      links: [
-        {url: '/ambassasor/list', text: 'View List'},
-        {url: '/ambassasors/import', text: 'Import'},
-      ]
-    },
-    {
-      icon: 'groups',
-      name: 'Staff',
-      links: [
-        {url: '/staff/list', text: 'View List'},
-        {url: '/staff/import', text: 'Import'},
-      ]
-    },
-    {
-      icon: 'attach_money',
-      name: 'Donors',
-      links: [
-        {url: '/donors/list', text: 'View List'},
-        {url: '/donors/import', text: 'Import'},
-      ]
-    },
-    {
-      icon: 'join_inner',
-      name: 'Matches',
-      links: [
-        {url: '/match/list', text: 'View List'},
-      ]
-    },
-  ];
+  title = 'thank-you-matcher-frontend';
 }
