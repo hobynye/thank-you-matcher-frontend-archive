3da90440966336c7548bf044f2737af41bef8844
Timothy Walshjamin <walshta@users.noreply.github.com>
2024-05-30 01:19:08 -0400

    feat: upload workflow (#272)
    * feat: Upgrade to Angular 17
    
    Upgrade to using Angular 17 and standalone components.
    
    * Feat/ambassor upload (#280)
    
    * Add admin dashboard to the app
    
    * Service for retrieving Ambassadors from the server
    
    * Admin dashboard

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,54 +1,63 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
-import { MatListModule } from "@angular/material/list";
-import { MatExpansionModule } from "@angular/material/expansion";
-
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Component } from "@angular/core";
+import { provideRouter, RouterLink } from "@angular/router";
+import { By } from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
-
+@Component({standalone: true, selector: 'router-outlet', template: ''})
+class RouterOutletStubComponent {}
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
+        AppComponent,
+        RouterLink,
+        RouterOutletStubComponent,
         BrowserAnimationsModule,
-        MatToolbarModule,
-        MatSidenavModule,
-        MatButtonModule,
-        MatIconModule,
-        MatListModule,
-        MatExpansionModule,
       ],
-      declarations: [
-        AppComponent
-      ],
+      providers: [
+        provideRouter([])
+      ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Thank You Matcher'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+  it(`should have the 'Thank You Matcher' title`, () => {
     expect(app.title).toEqual('Thank You Matcher');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should render toolbar', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.toolbar h2')?.textContent).toContain('Thank You Matcher');
+    expect(compiled.querySelector('.title')?.textContent).toContain('Thank You Matcher');
+  });
+
+  it('should render navigation', () => {
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const navSidePanel = compiled.querySelector('.nav');
+
+    expect(navSidePanel?.children.length).toEqual(1);
+    expect(navSidePanel?.children[0].textContent).toContain('Admin');
+  });
+
+  it('should get RouterLinks from template', () => {
+    fixture.detectChanges();
+    const linkDes = fixture.debugElement.queryAll(By.directive(RouterLink));
+    let routerLinks = linkDes.map((de) => de.injector.get(RouterLink));
+
+    expect(routerLinks.length).withContext('should have 1 routerLinks').toBe(1);
+    expect(routerLinks[0].href).toBe('/admin');
   });
 });
